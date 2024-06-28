d394ac5717dfd33df02ae88e533abecbb70f6f4e
Andy D <8628078+rpigu-i@users.noreply.github.com>
2024-06-28 13:33:10 +0000

    Add codeql codesapces prep script

---
+++ part1/codeqldb.sh
@@ -0,0 +1,5 @@
+zip -r javascript-database.zip /workspaces/javascript-database
+mv javascript-database.zip hope-sec-dev-workshop-2024/
+git add javascript-database.zip
+git commit -m"Adding our test database for CodeQL"
+git push
